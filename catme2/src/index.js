d91ae96250f3b116866ffa2243b46516ac43a5cf
Alex Ang <zang0001@student.monash.edu>
2019-08-21 01:36:28 +1000

    add firebase config
    Co-authored-by: Meherwan <msgil4@student.monash.edu>

--- catme2/src/index.js
+++ catme2/src/index.js
@@ -1,10 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import * as firebase from "firebase";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+var config = {
+  apiKey: "AIzaSyDKRjElKWwcW9RGWTqjjAeMPyDXDY3ew5E",
+  authDomain: "catem2pointoh.firebaseapp.com",
+  databaseURL: "https://catem2pointoh.firebaseio.com",
+  projectId: "catem2pointoh",
+  storageBucket: "",
+  messagingSenderId: "807684499073",
+  appId: "1:807684499073:web:64cf00c25b68b4ef"
+};
+
+firebase.initializeApp(config);
+
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

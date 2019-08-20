e129838b1c7be65a95d406f3c246d56624fee854
Meherwan <msgil4@student.monash.edu>
2019-08-21 00:28:18 +1000

    added config file

---
+++ catme2/src/firebase/config.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyDKRjElKWwcW9RGWTqjjAeMPyDXDY3ew5E",
+  authDomain: "catem2pointoh.firebaseapp.com",
+  databaseURL: "https://catem2pointoh.firebaseio.com",
+  projectId: "catem2pointoh",
+  storageBucket: "",
+  messagingSenderId: "807684499073",
+  appId: "1:807684499073:web:64cf00c25b68b4ef"
+};
+
+export default firebaseConfig;

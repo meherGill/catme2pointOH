2205e1dcc9f3a3753fa582ec608d4b74f5763335
Alex Ang <zang0001@student.monash.edu>
2019-08-24 14:06:35 +1000

    base of lab list

--- catme2/src/components/LabList.jsx
+++ catme2/src/components/LabList.jsx
@@ -1,7 +1,22 @@
-import React from 'react';
+import React from "react";
+import { Route } from "react-router-dom";
 
-const LabList = () => {
-  return <div>LabList</div>;
+const LabList = props => {
+  const lab = [];
+
+  const handleClick = index => {
+    this.props.history.push("/allocate", { key: index });
+  };
+
+  for (const [index, value] of props.entries()) {
+    lab.push(
+      <li key={index} onClick={() => handleClick(index)}>
+        {value}
+      </li>
+    );
+  }
+
+  return <div> {lab} </div>;
 };
 
 export default LabList;

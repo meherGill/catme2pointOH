2cf5a20d521c83f451f2447a5a610e137cec297c
Alex Ang <zang0001@student.monash.edu>
2019-08-24 18:12:12 +1000

    Update lablist

--- catme2/src/App.js
+++ catme2/src/App.js
@@ -1,23 +1,25 @@
-import React, { Fragment } from 'react';
-import './App.css';
+import React, { Fragment } from "react";
+import "./App.css";
 
-import { Container } from 'react-bootstrap';
+import { Container } from "react-bootstrap";
 
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, BrowserRouter } from "react-router-dom";
 
-import LabList from './components/LabList';
-import Navbar from './components/NavbarComponent';
-import Allocate from './components/Allocate';
+import LabList from "./components/LabList";
+import Navbar from "./components/NavbarComponent";
+import Allocate from "./components/Allocate";
 
 function App() {
   return (
     <Fragment>
       <Navbar />
       <Container>
-        <Switch>
-          <Route exact path='/' component={LabList} />
-          <Route path='/allocate' component={Allocate} />
-        </Switch>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={LabList} />
+            <Route path="/allocate" component={Allocate} />
+          </Switch>
+        </BrowserRouter>
       </Container>
     </Fragment>
   );
--- catme2/src/components/LabList.jsx
+++ catme2/src/components/LabList.jsx
@@ -1,61 +1,66 @@
-import React from 'react';
-import { Container, Col, Row } from 'react-bootstrap';
-import Lab from './Lab';
+import React from "react";
+import { Container, Col, Row } from "react-bootstrap";
+import Lab from "./Lab";
+import { Route, Link, BrowserRouter as Router } from "react-router-dom";
+import { Redirect } from "react-router-dom";
 
 const labs = [
   {
-    name: 'Lab 1',
+    name: "Lab 1",
     assignments: [
-      'Assignment 1: Prototype for website',
-      'Assignment 2: Technical Report on Agile Process Management',
-      'Assignment 3: Milestone for website',
-      'Assignment 4: Demo of Final Application'
+      "Assignment 1: Prototype for website",
+      "Assignment 2: Technical Report on Agile Process Management",
+      "Assignment 3: Milestone for website",
+      "Assignment 4: Demo of Final Application"
     ]
   },
   {
-    name: 'Lab 2',
+    name: "Lab 2",
     assignments: [
-      'Assignment 1: Prototype for website',
-      'Assignment 2: Technical Report on Agile Process Management',
-      'Assignment 3: Milestone for website',
-      'Assignment 4: Demo of Final Application'
+      "Assignment 1: Prototype for website",
+      "Assignment 2: Technical Report on Agile Process Management",
+      "Assignment 3: Milestone for website",
+      "Assignment 4: Demo of Final Application"
     ]
   },
   {
-    name: 'Lab 3',
+    name: "Lab 3",
     assignments: [
-      'Assignment 1: Prototype for website',
-      'Assignment 2: Technical Report on Agile Process Management',
-      'Assignment 3: Milestone for website',
-      'Assignment 4: Demo of Final Application'
+      "Assignment 1: Prototype for website",
+      "Assignment 2: Technical Report on Agile Process Management",
+      "Assignment 3: Milestone for website",
+      "Assignment 4: Demo of Final Application"
     ]
   },
   {
-    name: 'Lab 4',
+    name: "Lab TEST",
     assignments: [
-      'Assignment 1: Prototype for website',
-      'Assignment 2: Technical Report on Agile Process Management',
-      'Assignment 3: Milestone for website',
-      'Assignment 4: Demo of Final Application'
+      "Assignment 1: Prototype for website",
+      "Assignment 2: Technical Report on Agile Process Management",
+      "Assignment 3: Milestone for website",
+      "Assignment 4: Demo of Final Application"
     ]
   }
 ];
-const LabList = props => {
-  const lab = [];
 
-  const handleClick = index => {
-    this.props.history.push("/allocate", { key: index });
+const LabList = props => {
+  //const lab = [];
+  const lab = labs;
+  const item = [];
+
+  const handleClick = key => {
+    console.log(key);
   };
 
-  for (const [index, value] of props.entries()) {
-    lab.push(
+  for (const [index, value] of lab.entries()) {
+    item.push(
       <li key={index} onClick={() => handleClick(index)}>
-        {value}
+        {value.name}
       </li>
     );
   }
 
-  return <div> {lab} </div>;
+  return <Container> {item} </Container>;
 };
 
 export default LabList;

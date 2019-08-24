01c5aab3b5c83d9b67e45060e08d6ca0862c7d4c
Mubtasim Mahmud <mubtasimmahmud20@gmail.com>
2019-08-25 00:31:25 +1000

    Added switch logic for rendering different components for different matching algorithms

--- catme2/src/components/Allocate.jsx
+++ catme2/src/components/Allocate.jsx
@@ -1,20 +1,76 @@
 import React, { Component } from 'react';
 import TeamList from './TeamList';
-import { Container, Col, Row } from 'react-bootstrap';
+import { Container, Col, Row, Dropdown } from 'react-bootstrap';
 import StudentList from './StudentList';
 
 class Allocate extends Component {
+  state = {
+    choice: '',
+  };
+
+  chooseAlgorithm = choice => {
+    console.log('called');
+    switch (choice) {
+      case 'Manual':
+        this.setState({
+          choice: 'Manual',
+        });
+        break;
+      default:
+        this.setState({
+          choice: '',
+        });
+        break;
+    }
+  };
+
+  renderTeams = () => {
+    switch (this.state.choice) {
+      case 'Manual':
+        return (
+          <Row>
+            <Col md={3}>
+              <StudentList />
+            </Col>
+            <Col md={9}>
+              <TeamList />
+            </Col>
+          </Row>
+        );
+      default:
+        return <div></div>;
+    }
+  };
+
   render() {
     return (
       <Container>
         <Row>
-          <Col md={3}>
-            <StudentList />
-          </Col>
-          <Col md={9}>
-            <TeamList />
-          </Col>
+          <Dropdown>
+            <Dropdown.Toggle variant="success" id="dropdown-basic">
+              Choose Algorithm
+            </Dropdown.Toggle>
+
+            <Dropdown.Menu>
+              <Dropdown.Item onClick={() => this.chooseAlgorithm('Manual')}>
+                Manual
+              </Dropdown.Item>
+              <Dropdown.Item onClick={() => this.chooseAlgorithm('Random')}>
+                Random
+              </Dropdown.Item>
+              <Dropdown.Item onClick={() => this.chooseAlgorithm('Balanced')}>
+                Balanced
+              </Dropdown.Item>
+              <Dropdown.Item
+                onClick={() => this.chooseAlgorithm('High Performing')}
+              >
+                High performing
+              </Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
         </Row>
+
+        {this.renderTeams()}
       </Container>
     );
   }
--- catme2/src/components/NavbarComponent.jsx
+++ catme2/src/components/NavbarComponent.jsx
@@ -4,15 +4,15 @@ import { LinkContainer } from 'react-router-bootstrap';
 
 const NavbarComponent = () => {
   return (
-    <Navbar bg='light' expand='lg'>
-      <Navbar.Brand href='#home'>React-Bootstrap</Navbar.Brand>
-      <Navbar.Toggle aria-controls='basic-navbar-nav' />
-      <Navbar.Collapse id='basic-navbar-nav'>
-        <Nav className='mr-auto'>
-          <LinkContainer exact to='/'>
+    <Navbar class="navbar-component" bg="dark" expand="lg">
+      <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <LinkContainer exact to="/">
             <Nav.Link>Labs</Nav.Link>
           </LinkContainer>
-          <LinkContainer to='/allocate'>
+          <LinkContainer to="/allocate">
             <Nav.Link>Allocate</Nav.Link>
           </LinkContainer>
         </Nav>

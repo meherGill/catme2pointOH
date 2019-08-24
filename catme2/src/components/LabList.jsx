966bb2afac733f8ad6e49c877ae273a3619fcc69
Meherwan <msgil4@student.monash.edu>
2019-08-24 21:37:50 +1000

    merged more

--- catme2/src/components/LabList.jsx
+++ catme2/src/components/LabList.jsx
@@ -1,50 +1,51 @@
-import React from "react";
-import { Container, Col, Row } from "react-bootstrap";
-import Lab from "./Lab";
-import { Route, Link, BrowserRouter as Router } from "react-router-dom";
-import { Card, Button, ListGroup } from "react-bootstrap";
+import React from 'react';
+import { Container, Col, Row } from 'react-bootstrap';
+import Lab from './Lab';
+import Allocate from './Allocate';
+import { Card, Button, ListGroup } from 'react-bootstrap';
+import { Switch, Route, Link, BrowserRouter as Router } from 'react-router-dom';
 
 const labs = [
   {
-    id: "000",
-    name: "Lab 1",
+    id: '000',
+    name: 'Lab 1',
     assignments: [
-      "Assignment 1: Prototype for website",
-      "Assignment 2: Technical Report on Agile Process Management",
-      "Assignment 3: Milestone for website",
-      "Assignment 4: Demo of Final Application"
-    ]
+      'Assignment 1: Prototype for website',
+      'Assignment 2: Technical Report on Agile Process Management',
+      'Assignment 3: Milestone for website',
+      'Assignment 4: Demo of Final Application',
+    ],
   },
   {
-    id: "001",
-    name: "Lab 2",
+    id: '001',
+    name: 'Lab 2',
     assignments: [
-      "Assignment 1: Prototype for website",
-      "Assignment 2: Technical Report on Agile Process Management",
-      "Assignment 3: Milestone for website",
-      "Assignment 4: Demo of Final Application"
-    ]
+      'Assignment 1: Prototype for website',
+      'Assignment 2: Technical Report on Agile Process Management',
+      'Assignment 3: Milestone for website',
+      'Assignment 4: Demo of Final Application',
+    ],
   },
   {
-    id: "002",
-    name: "Lab 3",
+    id: '002',
+    name: 'Lab 3',
     assignments: [
-      "Assignment 1: Prototype for website",
-      "Assignment 2: Technical Report on Agile Process Management",
-      "Assignment 3: Milestone for website",
-      "Assignment 4: Demo of Final Application"
-    ]
+      'Assignment 1: Prototype for website',
+      'Assignment 2: Technical Report on Agile Process Management',
+      'Assignment 3: Milestone for website',
+      'Assignment 4: Demo of Final Application',
+    ],
   },
   {
-    id: "003",
-    name: "Lab TEST",
+    id: '003',
+    name: 'Lab TEST',
     assignments: [
-      "Assignment 1: Prototype for website",
-      "Assignment 2: Technical Report on Agile Process Management",
-      "Assignment 3: Milestone for website",
-      "Assignment 4: Demo of Final Application"
-    ]
-  }
+      'Assignment 1: Prototype for website',
+      'Assignment 2: Technical Report on Agile Process Management',
+      'Assignment 3: Milestone for website',
+      'Assignment 4: Demo of Final Application',
+    ],
+  },
 ];
 
 const LabList = props => {
@@ -59,30 +60,43 @@ const LabList = props => {
 
   for (const [index, value] of lab.entries()) {
     item.push(
-      <Card>
-        <Card.Body>
-          <Card.Title>{value.name}</Card.Title>
-          <Card.Text>
-            <ListGroup>
-              {value.assignments.map((data, index) => (
-                <ListGroup.Item
-                  action
-                  onClick={() => handleClick(index, value.id)}
-                >
-                  {data}
-                </ListGroup.Item>
-              ))}
-              {/* {value.assignments.map((value, index) => {
+      <Link to="/Allocate">
+        <Card>
+          <Card.Body>
+            <Card.Title>{value.name}</Card.Title>
+            <Card.Text>
+              <ListGroup>
+                {value.assignments.map((data, index) => (
+                  <ListGroup.Item
+                    action
+                    onClick={() => handleClick(index, value.id)}
+                  >
+                    {data}
+                  </ListGroup.Item>
+                ))}
+                {/* {value.assignments.map((value, index) => {
                 return <li key={index}>{value}</li>;
               })} */}
-            </ListGroup>
-          </Card.Text>
-        </Card.Body>
-      </Card>
+              </ListGroup>
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </Link>
     );
   }
 
-  return <Container> {item} </Container>;
+  return (
+    <Container>
+      <Switch>
+        <Route path="/Allocate" component={Allocate} />
+      </Switch>
+      {item}
+    </Container>
+  );
 };
-
+// <li key={index} onClick={() => handleClick(index)} >
+{
+  /* <Link to = "/Allocate">{value.name}</Link> */
+}
+// </li>
 export default LabList;
